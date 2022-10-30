fd6d11689aaa8dc8416f147e27eecd5ad3c6f8c9
Hiruni Perera <hiruni.p@eyepax.com>
2022-10-30 17:25:25 +0530

    add service page styling

--- src/pages/service/index.js
+++ src/pages/service/index.js
@@ -1,32 +1,46 @@
 import React from "react";
 
-import { Box, Grid, Button } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
+
+import { Button } from "@mui/material";
+import "../../assets/styles/service.css";
 
 const image = require("../../assets/images/services/haircut.png");
 
 const Services = () => {
   return (
     <div>
-      <Box >
-        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-          <Grid item xs={6}>
-            <h1>Haircut</h1>
-            <p>
-              At Prauge we are passionate about making people feel good while
-              looking their best. Attending advanced education allows us to keep
-              up with the latest trends and provide each guest with a unique &
-              customizable result. For your convenience, Schedule your
-              reservation today!
-            </p>
-            <Button sx={{ bgcolor: "button.primary", color: "#fff", textTransform: 'capitalize'}}>
-              Make a Reservation
-            </Button>
-          </Grid>
-          <Grid item xs={6}>
-            <img src={image} alt="serviceImage" style={{maxHeight: "80%"}}/>
-          </Grid>
+      <Grid container spacing={2}>
+        <Grid
+          item
+          xs={6}
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="flex-start"
+        >
+          <h1>Haircut</h1>
+          <p>
+            At Prauge we are passionate about making people feel good while
+            looking their best. Attending advanced education allows us to keep
+            up with the latest trends and provide each guest with a unique &
+            customizable result. For your convenience, Schedule your reservation
+            today!
+          </p>
+          <Button
+            sx={{
+              bgcolor: "button.primary",
+              color: "#fff",
+              textTransform: "capitalize",
+            }}
+          >
+            Make a Reservation
+          </Button>
         </Grid>
-      </Box>
+        <Grid item xs={6} className="service_image">
+          <img src={image} alt="serviceImage" />
+        </Grid>
+      </Grid>
     </div>
   );
 };
