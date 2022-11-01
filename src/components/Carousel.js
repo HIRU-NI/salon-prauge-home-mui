0359bfcb14a5a56a3bab90438bf09a9e740d5e56
Hiruni Perera <hiruni.p@eyepax.com>
2022-11-01 10:54:17 +0530

    add styling to carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,4 +1,10 @@
-import { Card, CardContent, Typography } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  Button,
+} from "@mui/material";
 import React from "react";
 
 import Slider from "react-slick";
@@ -6,24 +12,76 @@ import Slider from "react-slick";
 //custom-css
 import "../assets/styles/carousel.css";
 
-
-
 const settings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 2,
-  slidesToScroll: 2,
+  dots: false,
+  infinite: false,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  initialSlide: 0,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 0,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 };
 
 const items = [
   {
-    name: "Random Name #1",
-    description: "Probably the most random thing you have ever seen!",
+    name: "Haircut",
+    description:
+      "Lorem ipsum dolor sit amet, conse adipiscing elit, sed do eiusmod tempor incididunt ut.",
+    image: require("../assets/images/services/haircut.png"),
   },
   {
-    name: "Random Name #2",
-    description: "Hello World!",
+    name: "Hair Styling",
+    description:
+      "Lorem ipsum dolor sit amet, conse adipiscing elit, sed do eiusmod tempor incididunt ut.",
+    image: require("../assets/images/services/haircut.png"),
+  },
+  {
+    name: "Makeup",
+    description:
+      "Lorem ipsum dolor sit amet, conse adipiscing elit, sed do eiusmod tempor incididunt ut.",
+    image: require("../assets/images/services/haircut.png"),
+  },
+  {
+    name: "Makeup",
+    description:
+      "Lorem ipsum dolor sit amet, conse adipiscing elit, sed do eiusmod tempor incididunt ut.",
+    image: require("../assets/images/services/haircut.png"),
+  },
+  {
+    name: "Makeup",
+    description:
+      "Lorem ipsum dolor sit amet, conse adipiscing elit, sed do eiusmod tempor incididunt ut.",
+    image: require("../assets/images/services/haircut.png"),
+  },
+  {
+    name: "Makeup",
+    description:
+      "Lorem ipsum dolor sit amet, conse adipiscing elit, sed do eiusmod tempor incididunt ut.",
+    image: require("../assets/images/services/haircut.png"),
   },
 ];
 
@@ -36,8 +94,22 @@ const ServiceCarousel = () => {
       <Slider {...settings} className="carousel">
         {items.map((item) => {
           return (
-            <Card key={item.name}>
-              <CardContent>Hello</CardContent>
+            <Card key={item.name} className="carousel_item">
+              <CardMedia
+                component="img"
+                height="140"
+                image={item.image}
+                alt="item.name"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {item.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {item.description}
+                </Typography>
+                <Button>MAKE A RESERVATION</Button>
+              </CardContent>
             </Card>
           );
         })}
